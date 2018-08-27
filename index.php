4b04d36000047b48051b42841d6e0a3156ebacd3
Santiago Bruno <sbrunogonzalez@csumb.edu>
2018-08-27 22:38:58 +0000

    Updating Index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Santiago Bruno';
+echo "<h1> Santiago Bruno </h1>";
 
 ?>
 </body>
