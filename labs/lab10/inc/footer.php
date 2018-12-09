27bf44ffb1b2b6c0b6e483035afee3af857b5ce6
Santiago Bruno <sbrunogonzalez@csumb.edu>
2018-12-09 01:44:54 +0000

    adding lab 10

---
+++ labs/lab10/inc/footer.php
@@ -0,0 +1,13 @@
+<hr>
+        <footer>
+            
+            CST336 Internet Programming. 2018
+            &copy; 2018 Santiago Bruno<br />
+            <strong> Disclaimer: </strong> The information in this website is fictitous. It's used for academic purposes only.
+            <br />
+            <img src = "img/csumb_logo.jpg" alt = "CSUMB logo" title = "This is the CSUMB logo" width = "50px" />
+            <img src = "img/buddy.png" alt = "Buddy verification logo" title = "This is the Buddy verification logo" width = "50px" />
+            
+        </footer>
+        
+        
--- labs/lab10/index.php
+++ labs/lab10/index.php
@@ -5,8 +5,12 @@
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>   
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>   
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <style>
             body {
                 text-align: center;
@@ -16,10 +20,70 @@
     </head>
     <body>
         
-	<!--Add main menu here -->
+	  <?php 
+	    include 'inc/header.php';
+	    
+	  ?>
+        <!-- Display Carousel here  -->
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+            <li data-target="#myCarousel" data-slide-to="4"></li>
+            <li data-target="#myCarousel" data-slide-to="5"></li>
+          </ol>
         
+          <!-- Wrapper for slides -->
+          <center>
+              <div class="carousel-inner">
+            <div class="item active">
+              <img src="img/alex.jpg" alt="Alex">
+            </div>
         
-        <div class="jumbotron">
-          <h1> CSUMB Animal Shelter</h1>
-          <h2> The "official" animal adoption website of CSUMB </h2>
+            <div class="item">
+              <img src="img/carl.jpg" alt="Carl">
+            </div>
+        
+            <div class="item">
+              <img src="img/charlie.jpg" alt="Charlie">
+            </div>
+            
+            <div class="item">
+              <img src="img/sally.jpg" alt="Sally">
+            </div>
+            
+            <div class="item">
+              <img src="img/samantha.jpg" alt="Samantha">
+            </div>
+            
+            <div class="item">
+              <img src="img/ted.jpg" alt="Ted">
+            </div>
+          </div>
+        
+          <!-- Left and right controls -->
+          <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
+        </center>
+          
+        
+        <br>
+        <a class="btn btn-outline-success" href="pets.php" role="button">Adopt Now</a>
+        <br><br><br>
+        <?php
+        include 'inc/footer.php';
+        
+        ?>
+        </body>
+
+</html>
